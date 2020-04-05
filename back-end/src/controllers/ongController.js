08b63593d5b51dcf09e9ef84228795b510c8cf24
agricio <nerovigiann@hotmail.com>
2020-04-04 21:18:24 -0400

    add validation from back-end, tool for tests

--- back-end/src/controllers/ongController.js
+++ back-end/src/controllers/ongController.js
@@ -1,4 +1,5 @@
 const connection = require('../database/connection');
+const genereateUniqueId = require('../utils/generateUniqueId');
 const crypto = require('crypto');
 
 module.exports = {
@@ -13,7 +14,7 @@ module.exports = {
   async create( req, res) {
     const {name, email, whatsapp, city, uf} = req.body;
 
-    const id = crypto.randomBytes(4).toString('HEX');
+    const id = genereateUniqueId(); 
 
     await connection('ongs').insert({
         id,
